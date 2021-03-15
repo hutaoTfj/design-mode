9ef00f46089975f38e5da254287b5dad799aec33
晓寻遥 <2414776185@qq.com>
2021-03-15 16:12:31 +0800

    设计模式第三节: 建造者模式，实例工程代码

---
+++ hcy-design-mode-builder-mode-3-01/src/test/java/com/hcy/AppTest.java
@@ -0,0 +1,31 @@
+package com.hcy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+
+    @Test
+    public void test_DecorationPackageController(){
+        DecorationPackageController decoration = new DecorationPackageController();
+        // 豪华欧式
+        System.out.println(decoration.getMatterList(new BigDecimal("132.52"),1));
+        // 轻奢田园
+        System.out.println(decoration.getMatterList(new BigDecimal("98.25"),2));
+        // 现代简约
+        System.out.println(decoration.getMatterList(new BigDecimal("85.43"),3));
+    }
+}
---
+++ hcy-design-mode-builder-mode-3-02/src/test/java/com/hcy/AppTest.java
@@ -0,0 +1,29 @@
+package com.hcy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+
+    @Test
+    public void test_Builder(){
+        Builder builder = new Builder();
+        // 豪华欧式
+        System.out.println(builder.levelOne(132.52D).getDetail());
+        // 轻奢田园
+        System.out.println(builder.levelTwo(98.25D).getDetail());
+        // 现代简约
+        System.out.println(builder.levelThree(85.43D).getDetail());
+    }
+}
