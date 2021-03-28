d3dfc3500d5744c3dfaca027ef309a3073bb73e1
xiaoxunyao <huchengyea@163.com>
2021-03-28 10:11:29 +0800

    设计模式第九节：装饰器模式，项目工程代码

---
+++ hcy-design-mode-decorator-mode-9-01/src/test/java/com/hcy/AppTest.java
@@ -0,0 +1,19 @@
+package com.hcy;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        LoginSsoDecorator ssoDecorator = new LoginSsoDecorator();
+        String request = "1successxiaoxunyao";
+        boolean success = ssoDecorator.preHandle(request, "ewcdqwt40liuiu", "t");
+        System.out.println("登录校验：" + request + (success ? " 放行" : " 拦截"));
+    }
+}
---
+++ hcy-design-mode-decorator-mode-9-02/src/test/java/com/hcy/AppTest.java
@@ -0,0 +1,21 @@
+package com.hcy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        LoginSsoDecorator ssoDecorator = new LoginSsoDecorator(new SsoInterceptor());
+        String request = "1successxiaoxunyao";
+        boolean success = ssoDecorator.preHandle(request, "ewcdqwt40liuiu", "t");
+        System.out.println("登录校验：" + request + (success ? " 放行" : " 拦截"));
+    }
+}
