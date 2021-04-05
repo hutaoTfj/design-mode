7c681e794dff5ede3df9bc5d4c5d3ce355ae9a5f
xiaoxunyao <huchengyea@163.com>
2021-04-06 07:46:05 +0800

    设计模式第十节：外观模式，工程实例代码

---
+++ hcy-desgin-mode-facade-patterns-10-00/src/test/java/com/hcy/code/HcyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcy.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
